e2c294b98b1d1945888e1dcb0131f0d3da2edd2e
Tuong Nguyen <tuongnm2001@gmail.com>
2022-11-12 15:32:49 +0700

    video 18

--- src/views/Covid.js
+++ src/views/Covid.js
@@ -5,10 +5,11 @@ import moment from 'moment'
 const Covid = () => {
 
     const [dataCovid, setDataCovid] = useState([]);
-    const [loading, setLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(true)
+    const [isError, setIsErorr] = useState(false)
 
     useEffect(async () => {
-        setTimeout(async () => {
+        try {
             let res = await axios.get(`https://api.covid19api.com/country/vietnam?from=2022-10-12T00:00:00Z&to=2022-11-12T00:00:00Z`)
             let data = res && res.data ? res.data : []
             if (data && data.length > 0) {
@@ -19,8 +20,13 @@ const Covid = () => {
                 data = data.reverse();
             }
             setDataCovid(data)
-            setLoading(false)
-        }, 3000)
+            setIsLoading(false)
+            setIsErorr(false)
+        } catch (e) {
+            setIsErorr(true)
+            setIsLoading(false)
+        }
+
     }, [])
 
     return (
@@ -36,23 +42,30 @@ const Covid = () => {
                 </tr>
             </thead>
             <tbody>
-                {loading === false && dataCovid && dataCovid.length > 0 &&
+                {isError === false && isLoading === false && dataCovid && dataCovid.length > 0 &&
                     dataCovid.map(item => {
                         return (
                             <tr key={item.ID}>
                                 <td>{item.Date}</td>
-                                <td>{item.ID}</td>
                                 <td>{item.Confirmed}</td>
+                                <td>{item.Active}</td>
                                 <td>{item.Deaths}</td>
                                 <td>{item.Recovered}</td>
                             </tr>
                         )
                     })
                 }
-                {loading === true
+                {isLoading === true
                     &&
                     <tr>
                         <td colSpan='5' style={{ 'textAlign': 'center' }}>Loading...</td>
+                    </tr>
+                }
+
+                {isError === true
+                    &&
+                    <tr>
+                        <td colSpan='5' style={{ 'textAlign': 'center' }}>Something Wrong...</td>
                     </tr>
                 }
             </tbody>
