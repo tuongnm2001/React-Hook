d87d95c23d2897cddbac0feb026b4c430fa7b113
Tuong Nguyen <tuongnm2001@gmail.com>
2022-11-13 10:03:37 +0700

    video 24 + 25

--- src/views/DetailBlog.js
+++ src/views/DetailBlog.js
@@ -1,10 +1,39 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
+import useFetch from "../customize/fetch";
+import './Blogs.scss'
 
 const DetailBog = () => {
     let { id } = useParams();
+    let history = useHistory();
+
+
+    const { data: dataBlogDetail, isLoading, isError }
+        = useFetch(`https://jsonplaceholder.typicode.com/posts/${id}`, false)
+
+
+    const handleBackData = () => {
+        history.push('/blog')
+    }
+
     return (
         <div>
-            Detail Blogs {id}
+            <>
+
+                <div><span onClick={handleBackData} style={{ cursor: 'pointer' }}>&lt; --Back</span></div>
+                <div className="blog-detail">
+                    {dataBlogDetail &&
+                        <>
+                            <div className="title">
+                                Blog ID : {id} --- {isLoading === true ? 'Loading data ...' : dataBlogDetail.title}
+                            </div>
+
+                            <div className="content">
+                                {dataBlogDetail.body}
+                            </div>
+                        </>
+                    }
+                </div>
+            </>
         </div>
     )
 }
